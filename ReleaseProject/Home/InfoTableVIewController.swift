6e2e90e9f66ed90a505152bbc0c5441dab6b0d81
SehunKang <andriy.kang@gmail.com>
2021-12-20 15:18:21 +0900

    정보 뷰 완성, archive 시작

--- ReleaseProject/Home/InfoTableVIewController.swift
+++ ReleaseProject/Home/InfoTableVIewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import WebKit
 
 class InfoTableVIewController: UITableViewController {
 
@@ -14,6 +15,7 @@ class InfoTableVIewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "infoView_title".localized(withComment: "")
         self.tableView = UITableView(frame: self.tableView.frame, style: .insetGrouped)
         tableView.register(InfoTableViewCell.self, forCellReuseIdentifier: InfoTableViewCell.identifier)
         
@@ -24,15 +26,12 @@ class InfoTableVIewController: UITableViewController {
     }
     
     
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return infoTableTitle.count
     }
 
@@ -46,10 +45,75 @@ class InfoTableVIewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
+        return 66
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 0 {
+            if indexPath.item == 1 {
+                let vc = LicenseNotificationViewController()
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }
     }
 
 }
+
+class LicenseNotificationViewController: UITableViewController {
+    
+    var sortedLicenseList = openSourceList.sorted(by: {$0.0 < $1.0})
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "opensource_license".localized(withComment: "")
+        tableView.register(InfoTableViewCell.self, forCellReuseIdentifier: InfoTableViewCell.identifier)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        openSourceList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: InfoTableViewCell.identifier, for: indexPath) as? InfoTableViewCell else { return UITableViewCell() }
+        cell.label.text = sortedLicenseList[indexPath.item].key
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        55
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = WebView()
+        vc.licenseUrl = sortedLicenseList[indexPath.item].value
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+class WebView: UIViewController, WKNavigationDelegate {
+    
+    let webView = WKWebView()
+    var licenseUrl: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.navigationDelegate = self
+        view = webView
+        let url = URL(string: licenseUrl!)
+        webView.load(URLRequest(url: url!))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+    
+}
+
 
 class InfoTableViewCell: UITableViewCell {
     
@@ -67,6 +131,10 @@ class InfoTableViewCell: UITableViewCell {
             make.edges.equalTo(contentView.safeAreaLayoutGuide).inset(10)
         }
         label.font = .systemFont(ofSize: 20)
+        
+        preservesSuperviewLayoutMargins = false
+        separatorInset = UIEdgeInsets.init(top: 0, left: 20, bottom: 0, right: 20)
+        layoutMargins = UIEdgeInsets.init(top: 0, left: 20, bottom: 0, right: 20)
     }
     
     required init?(coder: NSCoder) {
@@ -74,3 +142,4 @@ class InfoTableViewCell: UITableViewCell {
     }
     
 }
+
---
+++ ReleaseProject/Home/OpensourceList.swift
@@ -0,0 +1,16 @@
+//
+//  OpensourceList.swift
+//  ReleaseProject
+//
+//  Created by Sehun Kang on 2021/12/20.
+//
+
+import Foundation
+
+
+let openSourceList = [
+    "Charts": "https://github.com/danielgindi/Charts/blob/master/LICENSE",
+    "Realm": "https://github.com/realm/realm-swift/blob/master/LICENSE",
+    "SnapKit": "https://github.com/SnapKit/SnapKit/blob/develop/LICENSE",
+    "Firebase-ios-sdk": "https://github.com/firebase/firebase-ios-sdk/blob/master/LICENSE"
+]
