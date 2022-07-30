23a63f0eeb30aa5c7ef34b532f1fcde4bef113b2
SehunKang <andriy.kang@gmail.com>
2022-07-30 23:22:52 +0900

    att 삭제

--- ReleaseProject/SceneDelegate.swift
+++ ReleaseProject/SceneDelegate.swift
@@ -53,29 +53,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		// Called when the scene has moved from an inactive state to an active state.
 		// Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
 		
-		DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-			
-			//ATT Framework
-			ATTrackingManager.requestTrackingAuthorization { status in
-				switch status {
-				case .notDetermined:
-					print("notDetermined")
-					Analytics.setAnalyticsCollectionEnabled(false)
-				case .restricted:
-					print("restricted")
-					Analytics.setAnalyticsCollectionEnabled(false)
-				case .denied:
-					print("denied")
-					Analytics.setAnalyticsCollectionEnabled(false)
-				case .authorized:
-					print("authorized") // 애널리틱스 수집 가능
-					Analytics.setAnalyticsCollectionEnabled(true)
-				@unknown default:
-					print("unknown")
-					Analytics.setAnalyticsCollectionEnabled(false)
-				}
-			}
-		}
 		
 	}
 
