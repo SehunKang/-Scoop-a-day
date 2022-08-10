8fc796385f45ad34ceb1cf520029f3c6d1df88e0
SehunKang <andriy.kang@gmail.com>
2022-08-10 16:58:42 +0900

    수정 버튼 완료

--- ReleaseProject/Calendar/CalendarViewController.swift
+++ ReleaseProject/Calendar/CalendarViewController.swift
@@ -19,13 +19,21 @@ class CalendarViewController: UIViewController, StoryboardView {
     @IBOutlet weak var urineLabel: UILabel!
     
     @IBOutlet weak var poopPlusButton: UIButton!
+    @IBOutlet weak var poopMinusButton: UIButton!
+    @IBOutlet weak var urinePlusButton: UIButton!
+    @IBOutlet weak var urineMinusButton: UIButton!
+    
+    @IBOutlet weak var editButton: UIButton!
     
     var disposeBag: DisposeBag = DisposeBag()
     
+    var isEditingCount = BehaviorRelay<Bool>(value: false)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
+        editingUIConfigure()
+    
     }
     
     init(provider: ServiceProviderType) {
@@ -58,6 +66,27 @@ class CalendarViewController: UIViewController, StoryboardView {
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
+        poopMinusButton.rx.tap
+            .map { _ in
+                Reactor.Action.countChangeButtonClicked(.poopMinus)
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        urinePlusButton.rx.tap
+            .map { _ in
+                Reactor.Action.countChangeButtonClicked(.urinePlus)
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        urineMinusButton.rx.tap
+            .map { _ in
+                Reactor.Action.countChangeButtonClicked(.urineMinus)
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
         reactor.state.asObservable().map { $0.catName }
             .bind(to: navigationItem.rx.title)
             .disposed(by: disposeBag)
@@ -77,9 +106,46 @@ class CalendarViewController: UIViewController, StoryboardView {
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
+        
     }
     
-
-
+    func editingUIConfigure() {
+        
+        editButton.rx.tap
+            .subscribe {[weak self] _ in
+                guard let value = self?.isEditingCount.value else {
+                    return
+                }
+                self?.isEditingCount.accept(!value)
+                
+            }
+            .disposed(by: disposeBag)
+        
+        isEditingCount
+            .map({ bool in
+                !bool
+            })
+            .bind(to: poopPlusButton.rx.isHidden,
+                  poopMinusButton.rx.isHidden,
+                  urinePlusButton.rx.isHidden,
+                  urineMinusButton.rx.isHidden
+            )
+            .disposed(by: disposeBag)
+        
+        isEditingCount.subscribe {[weak self] event in
+            guard let bool = event.element else {return}
+            if bool == true {
+                self?.editButton.setTitle(Localization.HomeView.done, for: .normal)
+                self?.editButton.setImage(nil, for: .normal)
+            } else {
+                self?.editButton.setTitle("", for: .normal)
+                self?.editButton.setImage(UIImage(systemName: "pencil.circle"), for: .normal)
+            }
+        }
+        .disposed(by: disposeBag)
+        
+    }
+    
+    
 }
 
